15b598668f7e6306e6127c5d1f33fd1c22d27aa3
flukowski <>
2021-04-22 11:06:23 +0530

    Add assets

--- bot.js
+++ bot.js
@@ -35,17 +35,20 @@ function onMessageHandler (target, context, msg, self) {
     const num = rollDice(commandName);
     client.say(target, `You rolled a ${num}`);
     console.log(`* Executed ${commandName} command`);
-  } if (commandName === '!frames') {
+  } else if (commandName === '!frames') {
     client.say(target, `https://interpoint-station.itch.io/intercorp`);
     console.log(`* Executed ${commandName} command`);
-  } if (commandName === '!discord') {
+  } else if (commandName === '!discord') {
     client.say(target, `https://discord.gg/pBvrH77D`);
     console.log(`* Executed ${commandName} command`);
-  } if (commandName === '!patreon') {
+  } else if (commandName === '!patreon') {
     client.say(target, `https://www.patreon.com/interpoint`);
     console.log(`* Executed ${commandName} command`);
-  } if (commandName === '!youtube') {
+  } else if (commandName === '!youtube') {
     client.say(target, `https://www.youtube.com/channel/UCV88ITZdBYnLpRGDFYXymKA/playlists`);
+    console.log(`* Executed ${commandName} command`);
+  } else if (commandName === '!assets') {
+    client.say(target, `https://interpoint-station.itch.io/lancer-rpg-pixel-art-maps-assets`);
     console.log(`* Executed ${commandName} command`);
   } else {
     console.log(`* Unknown command ${commandName}`);
